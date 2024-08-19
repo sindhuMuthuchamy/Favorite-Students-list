6adc2ab357142368a0d1df333d2662eb0323bb43
sindhuMuthuchamy <sindhumuthuchamy@gamil.com>
2024-08-19 11:47:52 +0530

    bg

--- src/components/FavouriteStudents.jsx
+++ src/components/FavouriteStudents.jsx
@@ -6,13 +6,13 @@ function FavouriteStudents() {
   const { favorites, removeFavorite } = useContext(FavoritesContext);
 
   return (
-    <div className="container mx-auto bg-pink-200 p-5 border rounded-md">
-      <h1 className="text-2xl font-bold mb-4">Favorite Students⭐</h1>
+    <div className="container mx-auto bg-pink-200 p-5 border rounded-md" style={{backgroundImage:'url("https://images2.alphacoders.com/130/1303532.png")'}}>
+      <h1 className="text-2xl font-bold mb-4 text-blue-200">Favorite Students⭐</h1>
       <ul className="list-disc pl-5">
         {favorites.length > 0 ? (
           favorites.map(student => (
-            <li key={student} className="mb-2 flex justify-between items-center">
-              <span className="text-lg">{student}</span>
+            <li key={student} className="mb-2 flex justify-between items-center text-white">
+              <span className="text-lg text-[#787055] font-bold">{student}</span>
               <button
                 onClick={() => removeFavorite(student)}
                 className="ml-4 px-4 py-2 bg-red-500 text-white font-bold rounded hover:bg-red-600"
@@ -22,10 +22,10 @@ function FavouriteStudents() {
             </li>
           ))
         ) : (
-          <p>No favorite students yet.</p>
+          <p className='text-[#787055] font-semibold'>No favorite students yet.</p>
         )}
       </ul>
-      <Link to="/" className="mt-4 inline-block font-bold text-pink-500 underline">
+      <Link to="/" className="mt-4 inline-block font-bold text-blue-400 underline" >
         Go to List of Students
       </Link>
     </div>
--- src/components/ListOfStudents.jsx
+++ src/components/ListOfStudents.jsx
@@ -1,26 +1,27 @@
 import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { FavoritesContext } from './FavouritesContext';
+// import {background} from '../images/Hogwarts.jpg';
 
 const studentsList = [
-  'Sindhu', 'Hasini', 'Priya', 'Thaslim', 'Suba','Bharathi'
+  'Hermione Granger', 'Harry Potter', 'Ron Weasley', 'Ginny Weasley', 'Cedric Diggory','Draco Malfoy'
 ];
 
 function ListOfStudents() {
   const { favorites, addFavorite } = useContext(FavoritesContext);
 
   return (
-    <div className="container mx-auto bg-slate-400 p-5 border rounded-md">
-      <h1 className="text-2xl font-bold mb-4">List of Students</h1>
+    <div className="container mx-auto bg-slate-400 p-5 border rounded-md h-full" style={{backgroundImage:'url("https://preview.redd.it/hogwarts-legacy-dark-edition-official-pc-wallpaper-v0-potn7vwaxyj91.png?width=1080&crop=smart&auto=webp&s=06c43132256143ee78313b2b576c8ab41b41b086")'}}>
+      <h1 className="text-2xl font-bold mb-4 text-[#5EE588]">List of Students</h1>
       <ul className="list-disc pl-5">
         {studentsList.map(student => (
-          <li key={student} className="mb-2 flex justify-between items-center">
+          <li key={student} className="mb-2 flex justify-between items-center text-[#C9E2C8] font-bold">
             <span className="text-lg">{student}</span>
             <button
               onClick={() => addFavorite(student)}
               disabled={favorites.includes(student)}
               className={`ml-4 px-4 py-2 text-white font-bold rounded ${
-                favorites.includes(student) ? 'bg-gray-600' : 'bg-black hover:bg-gray-900'
+                favorites.includes(student) ? 'bg-gray-600' : 'bg-[#787055] hover:bg-[#a89b71]'
               }`}
             >
               Add to ⭐
